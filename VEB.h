54eada0e5f57eb079af6b869df038c9a83d278e6
Ben Lischin <benlischin@gmail.com>
2025-01-24 18:35:07 -0500

    forget universe size

--- VEB.h
+++ VEB.h
@@ -13,46 +13,51 @@ class VEBInterface {
     public:
         virtual ~VEBInterface() {}
         virtual void Insert(uint32_t x) = 0;
+        // virtual void Delete(uint32_t x) = 0;
         virtual bool Query(uint32_t x) = 0;
         virtual std::pair<bool, uint32_t> Successor(uint32_t x) = 0;
+        virtual std::pair<bool, uint32_t> Predecessor(uint32_t x) = 0;
         virtual uint32_t Min() = 0;
         virtual uint32_t Max() = 0;
 };
 
-// with a base case universe size of 16, stop recurring and linearly search/manipulate a bitset
+// with a base case universe size, stop recurring and linearly search/manipulate a bitset
 class VEB_Base : public VEBInterface {
     public:
-        VEB_Base() : bitArray(0), min(0), max(0), isEmpty(true) {}
+        VEB_Base() : bitArray(0) {}
 
         uint32_t Min() override {
-            return min;
+            for (uint32_t i = 0; i < BASE_CASE; ++i) {
+                if (bitArray.test(i)) {
+                    return i;
+                }
+            }
+            return 0;
         }
 
         uint32_t Max() override {
-            return max;
+            for (int i = BASE_CASE - 1; i >= 0; --i) { // int to prevent unsigned underflow
+                if (bitArray.test(i)) {
+                    return i;
+                }
+            }
+            return 0;
         }
 
         void Insert(uint32_t x) override {
             bitArray.set(x);
-            if (isEmpty) {
-                min = max = x;
-                isEmpty = false;
-                return;
-            }
-            if (x < min) {
-                min = x;
-            } else if (x > max) {
-                max = x;
-            }
-            // or should we not maintain a min/max and just loop??
         }
+
+        // void Delete(uint32_t x) override {
+        //     bitArray.reset(x);
+        // }
 
         bool Query(uint32_t x) override {
             return bitArray.test(x);
         }
 
         std::pair<bool, uint32_t> Successor(uint32_t x) override {
-            if (x > max)  {
+            if (x > Max())  {
                 return {false, 0};
             }
             for (uint32_t i = x; i < 16; ++i) {
@@ -63,13 +68,22 @@ class VEB_Base : public VEBInterface {
             return {false, 0};
         }
 
+        std::pair<bool, uint32_t> Predecessor(uint32_t x) override {
+            if (x < Min())  {
+                return {false, 0};
+            }
+            for (int i = x; i >= 0; --i) { // int to prevent unsigned underflow
+                if (bitArray.test(i)) {
+                    return {true, i};
+                }
+            }
+            return {false, 0};
+        }
+
     private:
         // base case bit array
         std::bitset<BASE_CASE> bitArray;
-        uint32_t min;
-        uint32_t max;
-        bool isEmpty;
-
+        // avoid extra space of min/max/isEmpty fields, since the universe is small enough to quickly search. This also avoids extra operation to update them
 };
 
 // recursive Van Emde Boas tree
@@ -78,7 +92,7 @@ class VEB : public VEBInterface {
         // constructor given a fixed universe
         // assume universe of size 2^(2^n) so all nested vEB objects have perfect-square sizes
         // also assume we will not construct the original vEB tree with base case size 16
-        VEB(uint32_t u) : u(u), min(0), max(0), isEmpty(true) {
+        VEB(uint32_t u) : min(0), max(0), isEmpty(true) {
             // sqrt(u), rounded up to the nearest power of 2
             // rounding necessary since initial universe is UINT32_MAX = 2^32 - 1, not 2^32; all recursive universes will be perfect square powers of 2
             sqrtU = std::exp2(std::ceil(std::log2(u)/2));
@@ -104,7 +118,7 @@ class VEB : public VEBInterface {
         }
 
         // adds an element from the universe into the set
-        // space-efficient approach with lazy-loading nested clusters, at the time cost of extra heap allocations during insertions
+        // space-efficient approach with lazy-loading nested clusters, at the time cost of extra memory allocations during insertions
         void Insert(uint32_t x) {
             if (isEmpty) {
                 min = max = x;
@@ -125,24 +139,35 @@ class VEB : public VEBInterface {
             clusters[i]->Insert(low(x));
         }
 
-        // // removes an element in the universe from the set
+        // removes an element in the universe from the set
         // void Delete(uint32_t x) {
         //     if (isEmpty || (!isEmpty && (x < min || x > max))) { // nothing to delete
         //         return;
         //     }
         //     if (x == min && x == max) { // x is only item in this cluster
-        //         isEmpty = true;
         //         min = max = 0;
+        //         isEmpty = true;
         //         return;
         //         // can also make the clusters and summary nullptrs like reinforcing lazy loading
         //         // is it possible to make this cluster itself nullptr in the parent cluster?
         //     }
         //     if (x == min) {
         //         // min = next smallest
-                
-        //     }
-        //     if (x == max) {
-        //         // max = next largest
+        //         auto succ = Successor(x);
+        //         if (!succ.first) {
+        //             min = 0;
+        //             isEmpty = true;
+        //         } else {
+        //             min = succ.second;
+        //         }
+        //     } else if (x == max) {
+        //         auto prev = Predecessor(x);
+        //         if (!prev.first) {
+        //             max = 0;
+        //             isEmpty = true;
+        //         } else {
+        //             max = prev.second;
+        //         }
         //     }
         //     // x within some nested cluster
         //     clusters[high(x)]->Delete(low(x));
@@ -190,10 +215,35 @@ class VEB : public VEBInterface {
             return {true, index(i, j)};
         }
 
+        std::pair<bool, uint32_t> Predecessor(uint32_t x) {
+            if (isEmpty) {
+                return {false, 0};
+            }
+            if (x >= max) {
+                return {true, max};
+            }
+
+            uint32_t i = high(x);
+            uint32_t j;
+            if (clusters[i] != nullptr && low(x) >= clusters[i]->Min()) { // predecessor in same cluster
+                auto pred = clusters[i]->Predecessor(low(x));
+                // predecessor is known to exist, no need to check
+                j = pred.second;
+            } else { // no predecessor in this cluster
+                if (i <= summary->Min()) { // no prev nonempty clusters
+                    return {false, 0};
+                }
+                auto pred = summary->Predecessor(i-1); // find prev nonempty cluster
+                // predecessor is known to exist, no need to check
+                i = pred.second;
+                j = clusters[i]->Max();
+            }
+            return {true, index(i, j)};
+        }
+
     private:
-        // current-level universe size
-        uint32_t u;
         // number of next-level clusters; size of next-level clustrs
+        // reduce expensive square root calculations by performing it once in the constructor and reusing that value
         uint32_t sqrtU;
         // nested clusters
         std::vector<VEBInterface*> clusters;
